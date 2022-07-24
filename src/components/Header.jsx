e0919d4fb41400a8a24183b75678f27b1184d004
bitswa <bitswa@pm.me>
2022-07-24 15:03:22 -0300

    css main start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <div className="bg-blue-600 flex flex-col items-center">
       <h1 className="py-6 text-lg">IP Address Tracker</h1>
-      <form className="rounded-xl overflow-hidden flex w-[80%]">
+      <form className="rounded-xl overflow-hidden flex w-[90%]">
         <input className="p-2 w-full text-lg px-6 " type="text" />
         <button className="p-5 bg-black " type="submit">
           <img className="self-center" src="./images/icon-arrow.svg" alt="" />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,33 @@
 export function Main({ data }) {
   return (
     <div className="text-center">
-      {data?.map((address) => {
-        const {region, timezone,country} = address.location
-        return (
-          <main className="">
-            <div>
-              <h4>IP ADDRESS</h4>
-              <span>{address.ip}</span>
+      <main className="flex justify-center">
+        {data?.map((address) => {
+          const { region, timezone, country } = address.location;
+          return (
+            <div className="w-[90vw]">
+              <div>
+                <h4 className="text">IP ADDRESS</h4>
+                <span>{address.ip}</span>
+              </div>
+              <div>
+                <h4>LOCATION</h4>
+                <span>
+                  {region}, {country}
+                </span>
+              </div>
+              <div>
+                <h4>TIMEZONE</h4>
+                <span>UTC {timezone}</span>
+              </div>
+              <div>
+                <h4>ISP</h4>
+                <span>{address.isp}</span>
+              </div>
             </div>
-            <div>
-              <h4>LOCATION</h4>
-              <span>{region}, {country}</span>
-            </div>
-            <div>
-              <h4>TIMEZONE</h4>
-              <span>UTC {timezone}</span>
-            </div>
-            <div>
-              <h4>ISP</h4>
-              <span>{address.isp}</span>
-            </div>
-          </main>
-        );
-      })}
+          );
+        })}
+      </main>
     </div>
   );
 }
